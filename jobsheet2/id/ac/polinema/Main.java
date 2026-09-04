17025e90d92286e85ad8491a196c03895e4637f3
Fauziiiiii <fauzigoendoet99@gmail.com>
2026-09-04 09:42:25 +0700

    Jobsheet 3 - Langkah 7

--- jobsheet2/id/ac/polinema/Main.java
+++ jobsheet2/id/ac/polinema/Main.java
@@ -2,21 +2,21 @@ package jobsheet2.id.ac.polinema;
 
 public class Main {
     public static void main(String[] args) {
-        Rectangle original = new Rectangle(6, 4);
+        Rectangle[] shapes = new Rectangle[3];
+        shapes[0] = new Rectangle(6, 4);
+        shapes[1] = new Rectangle(3, 2);
+        shapes[2] = new Rectangle(8, 2);
 
-        System.out.println("Area: " + original.area());
-
-        Rectangle copy = original;
-        copy.width = 10;
-        System.out.println("Via original: " +original.area());
-        System.out.println("Via copy: " + copy.area());
+        for (Rectangle r : shapes) {
+            System.out.println("Area: " + r.area() + ", Perimeter: " + r.perimeter());
+        }
 
         Student s = new Student("Nadia", "S001", 3.8);
         System.out.println(s.describe());
 
-        // Area: 24
-        // Via original: 40
-        // Via copy: 40
+        // Area: 24, Perimeter: 20
+        // Area: 6, Perimeter: 10
+        // Area: 16, Perimeter: 20
         // Nadia (S001, GPA: 3.8)
     }
 }
