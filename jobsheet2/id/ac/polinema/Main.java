75dd19ffeb05e01930b240f0c79608a612cad588
Fauziiiiii <fauzigoendoet99@gmail.com>
2026-09-04 09:24:37 +0700

    Jobsheet 2 - Langkah 3, 4

--- jobsheet2/id/ac/polinema/Main.java
+++ jobsheet2/id/ac/polinema/Main.java
@@ -2,10 +2,14 @@ package jobsheet2.id.ac.polinema;
 
 public class Main {
     public static void main(String[] args) {
-        Rectangle r = new Rectangle();
+        Rectangle r = new Rectangle(6, 4);
         r.width = 6;
         r.height = 4;
 
-        System.out.println("Rectangle " + r.width + "x" + r.height);
+        System.out.println("Area: " + r.area());
+        System.out.println("Perimeter: " + r.perimeter());
+
+        // Area: 24
+        // Perimeter: 20
     }
 }
